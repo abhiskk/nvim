9765d16f2379940940f14d22da13684d258003b8
Abhishek Kadian <abhishekkadiyan@gmail.com>
2024-06-15 12:07:42 -0700

    latest changes

--- init.lua
+++ init.lua
@@ -5,47 +5,37 @@ vim.opt.wrap = true
 vim.opt.list = false
 vim.opt.linespace = 10
 
+-- Disable LSP semantic highlighting
 vim.lsp.handlers["textDocument/documentHighlight"] = function() end
 
 local vim = vim
 local api = vim.api
 
 -- change visual highlight color
--- api.nvim_set_hl(0, "Visual", { bg = "#FF9900" })
 api.nvim_set_hl(0, "Visual", { bg = "#A0522D" })
-
--- autosave
--- api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
---   pattern = "*",
---   command = "silent! write",
---   nested = true,
--- })
 
 -- remap x to only delete and not cut
 api.nvim_command([[nnoremap x "_x]])
 api.nvim_command([[xnoremap x "_x]])
 api.nvim_command([[set nostartofline]])
 
---increase scroll speed
+-- increase scroll speed
 api.nvim_command([[nnoremap <silent> <C-e> 5<C-e>]])
 api.nvim_command([[nnoremap <silent> <C-y> 5<C-y>]])
 
---change the highlight background
+-- change the highlight background
 api.nvim_set_hl(0, "Search", { bg = "#A0522D" })
 
--- api.nvim_command([[colorscheme kanagawa-wave]])
-api.nvim_command([[set background=dark]])
+-- Set Gruvbox colorscheme
+vim.o.termguicolors = true
+api.nvim_command([[set background=light]])
 api.nvim_command([[colorscheme gruvbox]])
 
--- api.nvim_set_hl(0, "NeoTreeNormal", { bg = "#A9BA9D" })
--- api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "#A9BA9D" })
--- api.nvim_set_hl(0, "NeoTreeRootName", { fg = "#C5A880", bold = true })
--- api.nvim_set_hl(0, "NeoTreeGitAdded", { fg = "#008080", bold = true })
--- api.nvim_set_hl(0, "NeoTreeGitChanged", { fg = "#008080", bold = true })
+-- Set NeoTree background
+-- api.nvim_set_hl(0, "NeoTreeNormal", { bg = "#000000" })
+-- api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "#000000" })
 
-api.nvim_set_hl(0, "NeoTreeNormal", { bg = "#000000" })
-api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "#000000" })
-
+-- Disable Treesitter highlighting for specific file types
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   pattern = { "*.txt", "*.md", "*.py" }, -- Add the file patterns you want to disable Treesitter for
   callback = function()
